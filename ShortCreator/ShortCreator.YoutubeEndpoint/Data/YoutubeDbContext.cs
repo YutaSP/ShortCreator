c0529950a5014ea8cd52bddc36d671d349c7b06a
FunionsCanGetIt <66400219+YutaSP@users.noreply.github.com>
2025-03-29 15:59:05 -0700

    update

--- ShortCreator/ShortCreator.YoutubeEndpoint/Data/YoutubeDbContext.cs
+++ ShortCreator/ShortCreator.YoutubeEndpoint/Data/YoutubeDbContext.cs
@@ -1,6 +1,44 @@
-﻿namespace ShortCreator.YoutubeEndpoint.Data
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ShortCreator.YoutubeEndpoint.Models;
+
+namespace ShortCreator.YoutubeEndpoint.Data;
+
+public partial class YoutubeDbContext : DbContext
 {
-    public class YoutubeDbContext
+    public YoutubeDbContext()
     {
     }
+
+    public YoutubeDbContext(DbContextOptions<YoutubeDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<YoutubeTargetVidId> YoutubeTargetVidIds { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<YoutubeTargetVidId>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Youtube_Target_Vid_Ids");
+
+            entity.Property(e => e.RedditId).IsUnicode(false);
+            entity.Property(e => e.VidPath)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ ShortCreator/ShortCreator.YoutubeEndpoint/Models/YoutubeTargetVidId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortCreator.YoutubeEndpoint.Models;
+
+public partial class YoutubeTargetVidId
+{
+    public int? VidId { get; set; }
+
+    public string? RedditId { get; set; }
+
+    public string? VidPath { get; set; }
+}
